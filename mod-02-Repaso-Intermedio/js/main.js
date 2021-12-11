a64639637867c0b461754e5f10f75af955d696bf
Mariamafalda2021 <mariarezgarcia@gmail.com>
2021-12-11 15:11:44 +0100

    function handlerUpdateState

--- mod-02-Repaso-Intermedio/js/main.js
+++ mod-02-Repaso-Intermedio/js/main.js
@@ -4,11 +4,32 @@ const fondoAmarillo = document.querySelector(".js-fondoAmarillo");
 
 const happyFace = document.querySelector(".js-happyFace");
 const sadFace = document.querySelector(".js-sadFace");
+const feeling = document.querySelector(".feeling")
+const option1 = document.querySelector(".option1");
+const option2 = document.querySelector(".option2");
 
 const chooseOption = document.querySelector(".js-chooseOption");
 
-function opcionCambiada() {
+const updateButton = document.querySelector(".update")
+
+function handlerChangeOption() {
     console.log("cambio");
+
+
 }
 
-chooseOption.addEventListener("change", opcionCambiada);
+function handlerUpdateState() {
+    console.log("click");
+    if (chooseOption.value === "happy") {
+        happyFace.classList.remove("hidden")
+        sadFace.classList.add("hidden");
+    } else {
+        happyFace.classList.add("hidden")
+        sadFace.classList.remove("hidden");
+    }
+}
+
+
+chooseOption.addEventListener("change", handlerChangeOption);
+
+updateButton.addEventListener("click", handlerUpdateState);
