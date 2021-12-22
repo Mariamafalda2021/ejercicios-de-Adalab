ab1d308424662ecfbfa8195ec30820f894b3be23
Mariamafalda2021 <mariarezgarcia@gmail.com>
2021-12-22 11:31:18 +0100

    lesson 13!

---
+++ modulo-02-leccion-13-ej-01/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*const marks = [5, 4, 6, 7, 9];
+const inflatedMarks = marks.map(function (x) {
+    return x + 1;
+});
+
+console.log(inflatedMarks);*/
+
+/*const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+const getUperCaseName = name => name.toUpperCase();
+const capitalNames = names.map(getUperCaseName);
+
+console.log(capitalNames);*/
+
+const marks = [5, 4, 6, 7, 9];
+const inflatedMarks = marks.map(mark => mark + 1);
+
+console.log(inflatedMarks);
