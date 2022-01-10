236ce98e55292a672bac728dcf4eb52a178da3f8
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-01-10 20:24:58 +0100

    modulo 3 leccion 1 ejercicio 1 y 2

--- modulo-3/modulo-3-leccion-01/src/components/App.js
+++ modulo-3/modulo-3-leccion-01/src/components/App.js
@@ -2,12 +2,39 @@ import '../styles/App.css';
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1 class="title">1.5 React y CSS</h1>
-      </header>
-    </div>
+    <div className="app">
+      <div className="head">
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?
+    family=Rubik:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="./css/style.css" />
+        <title>Evaluación Intermedia</title>
+      </div>
+
+      <div className="body">
+        <header className="header">
+          <a href="https://adalab.es"><img className="logo" src="../images/adalab-logo-155x61.png" alt="logo-adalab" /></a>
+          <nav>
+            <ul className="menu">
+              <li><a className="menu-item" href="#">Blog</a></li>
+              <li><a className="menu-item" href="#">Contacto</a></li>
+            </ul>
+
+          </nav>
+
+        </header>
+
+        <main className="main">
+          <h1 className="title">
+            Creando diversidad digital
+          </h1>
+
+        </main>
+      </div>
+    </div >
   );
 }
-
 export default App;
