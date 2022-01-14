987ac1399bc2be394b84bdaa803c9fbd4b615f33
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-01-14 19:51:22 +0100

    Lección 4.1 ejercicios 1 y 2

--- Modulo_3_React/leccion-04/ejercicio-1-2/src/components/App.js
+++ Modulo_3_React/leccion-04/ejercicio-1-2/src/components/App.js
@@ -1,9 +1,33 @@
 import '../styles/App.css';
 
 function App() {
+  const tasks = [
+    { task: 'Comprar harina, jamón y pan rallado', completed: true },
+    { task: 'Hacer croquetas ricas', completed: true },
+    { task: 'Ir a la puerta de un gimnasio', completed: false },
+    { task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio', completed: false }
+  ];
+
+  const renderTasks = () => {
+    return tasks.map((task, index) => {
+      if (task.completed === true) {
+        return (
+          <li key={index} className="completed"> {task.task}</li >
+        );
+      } else {
+        return (
+          <li key={index}> {task.task}</li >)
+      }
+    });
+  };
+
+
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      <h1>Mi lista de tareas</h1>
+      <ol>
+        {renderTasks()}
+      </ol>
     </div>
   );
 }
