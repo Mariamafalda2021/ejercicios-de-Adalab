427f2a4e91fba3e22c5960fde227fbfe5b06cb06
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-01-15 10:55:34 +0100

    leccion 4.2

--- Modulo_3_React/leccion-04/leccion-4_2-ej-1/src/components/App.js
+++ Modulo_3_React/leccion-04/leccion-4_2-ej-1/src/components/App.js
@@ -1,33 +1,52 @@
-import '../styles/App.css';
+import '../styles/App.scss';
+import { useState } from 'react';
 
 function App() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { task: 'Comprar harina, jamón y pan rallado', completed: true },
     { task: 'Hacer croquetas ricas', completed: true },
     { task: 'Ir a la puerta de un gimnasio', completed: false },
     { task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio', completed: false }
-  ];
+  ]);
 
-  const renderTasks = () => {
-    return tasks.map((task, index) => {
-      if (task.completed === true) {
-        return (
-          <li key={index} className="completed"> {task.task}</li >
-        );
-      } else {
-        return (
-          <li key={index}> {task.task}</li >)
-      }
-    });
+  const [userSearch, setUserSearch] = useState('');
+
+  const handleSubmit = (event) => event.preventDefault();
+
+  const handleUserSearch = (event) => {
+    setUserSearch(event.target.value);
   };
 
+  const handleClick = (event) => {
+    const clickedTask = event.currentTarget.id;
+    tasks[clickedTask].completed = !tasks[clickedTask].completed;
+    setTasks([...tasks]);
+  };
 
+  const renderTasks = () => {
+    tasks
+      .filter((task) => task.task.toLowerCase().includes(userSearch.toLowerCase()))
+      //Mapeamos
+      .map((task, index) => {
+        if (task.completed === true) {
+          return (
+            <li id={index} key={index} className="task task--completed" onClick={handleClick}> {task.task}</li >
+          );
+        } else {
+          return (
+            <li id={index} key={index} className="task" onClick={handleClick}> {task.task}</li >)
+        }
+      });
+  };
   return (
     <div className="App">
       <h1>Mi lista de tareas</h1>
       <ol>
         {renderTasks()}
       </ol>
+      <form onSubmit={handleSubmit}>
+        <input type="text" id="userSecarch" onChange={handleUserSearch} value={userSearch} />
+      </form>
     </div>
   );
 }
