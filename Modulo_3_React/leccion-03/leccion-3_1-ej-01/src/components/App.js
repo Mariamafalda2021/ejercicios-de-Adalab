1527f54571e6bf49215f0de6b6aba5501831b9aa
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-01-20 18:33:28 +0100

    ej 1

---
+++ Modulo_3_React/leccion-03/leccion-3_1-ej-01/src/components/App.js
@@ -0,0 +1,27 @@
+import '../styles/App.scss';
+import { Route, Link, Switch } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h2>Este título aparece siempre</h2>
+
+      <Route path="/contacto">
+        <h2>Este título solo aparece cuando la usuaria entra en la página de contacto</h2>
+      </Route>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Ir al inicio</Link>
+          </li>
+          <li>
+            <Link to="/contacto" className="link" id="contact-link" target="_blank">Ir a contacto</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
