e5f902b928a2e74ee1a2a8dbb9d70530455c2a9d
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-01-22 14:54:10 +0100

    3.2 ejercicio 1

---
+++ Modulo_3_React/leccion-03/leccion-3_2-ej-01/src/components/App.js
@@ -0,0 +1,93 @@
+import '../styles/App.scss';
+import { useState } from 'react';
+
+const App = () => {
+  // Creamos la constante de estado macarrones etc con el valor inicial a booleano
+  // Al iniciar suponemos que la usuaria no quiere macarrones etc, por eso ponemos false
+  const [macarrones, setMacarrones] = useState(false);
+  const [patatas, setPatatas] = useState(false);
+  const [huevos, setHuevos] = useState(false);
+  const [nueces, setNueces] = useState(false);
+  const [cebolla, setCebolla] = useState(false);
+  const [cerveza, setCerveza] = useState(false);
+
+  // Creamos la función manejadora
+  const handleMacarrones = ev => {
+    setMacarrones(ev.currentTarget.checked);// En la que guardamos no el valor del input sino su propiedad checked, en macarrones con setMacarrones
+  };
+  const handlePatatas = ev => {
+    setPatatas(ev.target.checked);
+  };
+  const handleNueces = ev => {
+    setNueces(ev.target.checked);
+  };
+  const handleHuevos = ev => {
+    setHuevos(ev.target.checked);
+  };
+  const handleCebolla = ev => {
+    setCebolla(ev.target.checked);
+  };
+  const handleCerveza = ev => {
+    setCerveza(ev.target.checked);
+  };
+
+  const handleAddAll = ev => {
+    setMacarrones(true);
+    setPatatas(true);
+    setNueces(true);
+    setHuevos(true);
+    setCebolla(true);
+    setCerveza(true);
+  }
+
+  const handleResetAll = () => {
+    setMacarrones(false);
+    setPatatas(false);
+    setNueces(false);
+    setHuevos(false);
+    setCebolla(false);
+    setCerveza(false);
+
+  }
+  const getMessage = () => {
+    let message = '';
+    if (patatas && huevos && cebolla) {
+      message = 'Eres una persona concebollista'
+    } else if
+      (macarrones || nueces || cerveza) {
+      message = 'Eres un robot sin paladar';
+    }
+    return message;
+  }
+
+  return (
+    <div>
+      <h1>Selecciona los ingredientes de la tortilla de patatas</h1>
+      <form>
+        <label htmlFor="ingrediente">
+          <input type="checkbox" name="ingrediente" id="Macarrones" onChange={handleMacarrones} />Macarrones
+        </label>
+        <label htmlFor="ingrediente">
+          <input type="checkbox" name="ingrediente" id="Patatas" onChange={handlePatatas} />Patatas
+        </label>
+        <label htmlFor="ingrediente">
+          <input type="checkbox" name="ingrediente" id="Nueces" onChange={handleNueces} />Nueces
+        </label>
+        <label htmlFor="ingrediente">
+          <input type="checkbox" name="ingrediente" id="Huevos" onChange={handleHuevos} />Huevos
+        </label>
+        <label htmlFor="ingrediente">
+          <input type="checkbox" name="ingrediente" id="Cebolla" onChange={handleCebolla} />Cebolla
+        </label>
+        <label htmlFor="ingrediente">
+          <input type="checkbox" name="ingrediente" id="Cerveza" onChange={handleCerveza} />Cerveza
+        </label>
+        <input type="button" value="marcar todos" onClick={handleAddAll} />
+        <input type="button" value="desmarcar todos" onClick={handleResetAll} />
+      </form>
+      <p>{getMessage()}</p>
+    </div>
+  );
+};
+
+export default App;
