961a05bed1e9f7b5ca7050327a204e4c1317ee17
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-01-23 15:07:59 +0100

    ejercicios

--- Modulo_3_React/leccion-08/leccion-8-ej-1/src/components/Header.js
+++ Modulo_3_React/leccion-08/leccion-8-ej-1/src/components/Header.js
@@ -1,13 +1,14 @@
 import Link from "./Link";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className="header">
             <a href="https://adalab.es"><img className="logo" alt="logo-adalab" /></a>
             <nav>
                 <ul className="menu">
-                    <Link text='Blog' href="https://adalab.es/blog/" tab="openInNewTab" />
-                    <li className="menu-item"><a className="menu-link" href="https://adalab.es/contacto/">Contacto</a></li>
+                    <Link text='Blog' href="https://adalab.es/blog/" openInNewTab={false} />
+                    <Link text='Contacto' href="https://adalab.es/contacto/" openInNewTab={true} />
+
                 </ul>
 
             </nav>
--- Modulo_3_React/leccion-08/leccion-8-ej-1/src/components/Link.js
+++ Modulo_3_React/leccion-08/leccion-8-ej-1/src/components/Link.js
@@ -1,7 +1,13 @@
 const Link = (props) => {
-    return (
-        <li className="menu-item"><a className="menu-link" href={props.href}>{props.text}</a></li>
+    if (props.openInNewTab) {
 
+
+        return (
+            <li><a target="_blank" className="menu-link" href={props.href}>{props.text}</a></li>
+        )
+    }
+    return (
+        <li><a className="menu-link" href={props.href}>{props.text}</a></li>
     )
 }
 export default Link;
